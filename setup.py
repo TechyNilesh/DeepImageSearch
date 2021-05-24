454d7916bb7e5a4f10c33b1f9f6bf446c46d4f70
TechyNilesh <49296182+TechyNilesh@users.noreply.github.com>
2021-05-24 12:32:20 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ setup(
   license='MIT',        
   description = 'Deep Image Search is an AI-based image search engine that includes deep transfor learning features Extraction and tree-based vectorized search.',
   author = 'Nilesh Verma',                   
-  author_email = 'me@nileshverma.com',      # Type in your E-Mail
-  url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['Deep Image Search Engine', 'AI Image search', 'Image Search Python'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
+  author_email = 'me@nileshverma.com',     
+  url = 'https://github.com/TechyNilesh/DeepImageSearch',   
+  download_url = 'https://github.com/TechyNilesh/DeepImageSearch/archive/refs/tags/v_01.tar.gz',    
+  keywords = ['Deep Image Search Engine', 'AI Image search', 'Image Search Python'],   
+  install_requires=[        
           'annoy',
           'matplotlib',
           'pandas',
