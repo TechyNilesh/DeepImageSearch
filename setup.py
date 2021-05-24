779c10941a2d12eeeb90771cd5785462eb89b0d1
TechyNilesh <49296182+TechyNilesh@users.noreply.github.com>
2021-05-24 18:06:56 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,4 @@
-from distutils.core import setup
-from os import path
-
-# The directory containing this file
-here = path.abspath(path.dirname(__file__))
-
-setup(from distutils.core import setup
+from setuptools import setup
 import pathlib
 
 
@@ -15,51 +9,17 @@ HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
 
 setup(
+  long_description_content_type="text/markdown",
   name = 'DeepImageSearch',         
   packages = ['DeepImageSearch'],
-  version = '0.7',
+  version = '1.0',
   license='MIT',        
   description = 'Deep Image Search is an AI-based image search engine that includes deep transfor learning features Extraction and tree-based vectorized search.',
   long_description=README,
-  long_description_content_type="text/markdown",
   author = 'Nilesh Verma',                   
   author_email = 'me@nileshverma.com',     
   url = 'https://github.com/TechyNilesh/DeepImageSearch',
-  download_url = 'https://github.com/TechyNilesh/DeepImageSearch/archive/refs/tags/v_07.tar.gz',    
-  keywords = ['Deep Image Search Engine', 'AI Image search', 'Image Search Python'],   
-  install_requires=[        
-          'annoy',
-          'matplotlib',
-          'pandas',
-          'numpy',
-          'tensorflow',
-          'tqdm',
-          'Pillow'
-      ],
-  classifiers=[
-    'Development Status :: 4 - Beta',
-    'Intended Audience :: Developers', 
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8',
-    'Programming Language :: Python :: 3.9',
-  ],
-)
-  name = 'DeepImageSearch',         
-  packages = ['DeepImageSearch'],
-  version = '0.6',
-  license='MIT',        
-  description = 'Deep Image Search is an AI-based image search engine that includes deep transfor learning features Extraction and tree-based vectorized search.',
-  long_description=open('README.rst', encoding="utf8").read(),
-  author = 'Nilesh Verma',                   
-  author_email = 'me@nileshverma.com',     
-  url = 'https://github.com/TechyNilesh/DeepImageSearch',
-  download_url = 'https://github.com/TechyNilesh/DeepImageSearch/archive/refs/tags/v_06.tar.gz',    
+  download_url = 'https://github.com/TechyNilesh/DeepImageSearch/archive/refs/tags/v_10.tar.gz',    
   keywords = ['Deep Image Search Engine', 'AI Image search', 'Image Search Python'],   
   install_requires=[        
           'annoy',
